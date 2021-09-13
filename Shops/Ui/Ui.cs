309adf94cc2bf470ed8e5179aedf8c19e68c5319
Ivan Evtushenko <eid20021@gmail.com>
2021-09-13 10:39:24 +0300

    refactor: Ui command handler create

--- Shops/Ui/Ui.cs
+++ Shops/Ui/Ui.cs
@@ -9,232 +9,47 @@ namespace Shops
 {
     public class Ui
     {
-        private ShopService _shopService;
-        private Buyer _buyer;
+        private CommandHandler _commandHandler;
 
         public Ui(ShopService shopService, Buyer buyer)
         {
-            _shopService = shopService;
-            _buyer = buyer;
+            _commandHandler = new CommandHandler(shopService, buyer);
         }
 
         public void Run()
         {
-            ChooseAction();
-        }
-
-        private void ChooseAction()
-        {
             while (true)
             {
                 AnsiConsole.Clear();
-                string action = AnsiConsole.Prompt(
-                    new SelectionPrompt<string>()
-                        .Title("[yellow]Choose action[/]")
-                        .PageSize(10)
-                        .AddChoices(new[]
-                        {
-                            "Register product",
-                            "Register shop",
-                            "Add product to the shop",
-                            "Change price",
-                            "Find cheapest shop",
-                            "Buy",
-                            "Show shop products",
-                            "Show shops",
-                            "Show registered products",
-                            "Show my products",
-                            "Show my money",
-                            "Add money",
-                            "exit",
-                        }));
-
-                switch (action)
-                {
-                    case "Register product":
-                        RegisterProduct();
-                        break;
-                    case "Register shop":
-                        RegisterShop();
-                        break;
-                    case "Add product to the shop":
-                        AddProductToShop();
-                        break;
-                    case "Change price":
-                        ChangePrice();
-                        break;
-                    case "Find cheapest shop":
-                        FindCheapestShop();
-                        break;
-                    case "Buy":
-                        Buy();
-                        break;
-                    case "Show shop products":
-                        ShowShopProducts();
-                        break;
-                    case "Show shops":
-                        ShowShops();
-                        break;
-                    case "Show registered products":
-                        ShowRegisteredProducts();
-                        break;
-                    case "Show my products":
-                        ShowBuyerProduct();
-                        break;
-                    case "Show my money":
-                        ShowBuyerMoney();
-                        break;
-                    case "Add money":
-                        AddBuyerMoney();
-                        break;
-                    case "exit":
-                        return;
-                }
+                string command = AskAction();
+                if (command == "exit")
+                    return;
+                _commandHandler.Run(command);
             }
         }
 
-        private void ShowBuyerProduct()
+        private string AskAction()
         {
-            var table = new ProductsTable();
-            table.AddProducts(_buyer.Products);
-            AnsiConsole.Render(table.Table);
-            ReturnPrompt();
-        }
-
-        private void ShowShops()
-        {
-            var table = new ShopsTable();
-            table.AddProducts(_shopService.Shops);
-            AnsiConsole.Render(table.Table);
-            ReturnPrompt();
-        }
-
-        private void RegisterProduct()
-        {
-            string name = AnsiConsole.Ask<string>("Enter [green]product name[/]?");
-            _shopService.RegisterProduct(name);
-        }
-
-        private void RegisterShop()
-        {
-            string name = AnsiConsole.Ask<string>("Enter [green]shop name[/]?");
-            string address = AnsiConsole.Ask<string>("Enter [green]shop address[/]?");
-            _shopService.RegisterShop(name, address);
-        }
-
-        private void ShowBuyerMoney()
-        {
-            AnsiConsole.Render(new Markup($"Buyer have [green]{_buyer.Money}[/]\n"));
-            ReturnPrompt();
-        }
-
-        private void AddBuyerMoney()
-        {
-            uint money = AnsiConsole.Ask<uint>("How much you want to add?");
-            _buyer.Money += money;
-        }
-
-        private void ShowRegisteredProducts()
-        {
-            var table = new CatalogProductsTable();
-            table.AddProducts(_shopService.Catalog);
-            AnsiConsole.Render(table.Table);
-            ReturnPrompt();
-        }
-
-        private void ShowShopProducts()
-        {
-            uint id = AnsiConsole.Ask<uint>("Enter [green]shop id[/]");
-            var table = new SellableProductsTable();
-            try
-            {
-                table.AddProducts(_shopService.GetShopProducts(id));
-            }
-            catch (ShopServiceException e)
-            {
-                AnsiConsole.Render(new Markup($"[red]{e.Message}[/]\n"));
-                ReturnPrompt();
-                return;
-            }
-
-            AnsiConsole.Render(table.Table);
-            ReturnPrompt();
-        }
-
-        private void AddProductToShop()
-        {
-            uint id = AnsiConsole.Ask<uint>("Enter [green]shop id[/]");
-            string productName = AnsiConsole.Ask<string>("Enter [green]product name[/]");
-            uint count = AnsiConsole.Ask<uint>("Enter [green]products count[/]");
-            uint price = AnsiConsole.Ask<uint>("Enter [green]product price[/]");
-            try
-            {
-                _shopService.AddProduct(id, productName, count, price);
-            }
-            catch (ShopServiceException e)
-            {
-                AnsiConsole.Render(new Markup($"[red]{e.Message}[/]\n"));
-                ReturnPrompt();
-            }
-        }
-
-        private void FindCheapestShop()
-        {
-            string productName = AnsiConsole.Ask<string>("Enter [green]product name[/]");
-            uint count = AnsiConsole.Ask<uint>("Enter [green]products count[/]");
-            uint shopId;
-            try
-            {
-                shopId = _shopService.FindCheapestPriceShopId(productName, count);
-            }
-            catch (ShopServiceException e)
-            {
-                AnsiConsole.Render(new Markup($"[red]{e.Message}[/]\n"));
-                ReturnPrompt();
-                return;
-            }
-
-            AnsiConsole.Markup(
-                $"Cheapest {productName} found in shop with id [green]{shopId}[/] for [green]{_shopService.GetShopProducts(shopId).FirstOrDefault(product => product.Name == productName).Price}[/]\n");
-            ReturnPrompt();
-        }
-
-        private void Buy()
-        {
-            uint id = AnsiConsole.Ask<uint>("Enter [green]shop id[/]");
-            string productName = AnsiConsole.Ask<string>("Enter [green]product name[/]");
-            uint count = AnsiConsole.Ask<uint>("Enter [green]product count[/]");
-            try
-            {
-                _shopService.Buy(_buyer, id, productName, count);
-            }
-            catch (ShopServiceException e)
-            {
-                AnsiConsole.Render(new Markup($"[red]{e.Message}[/]\n"));
-                ReturnPrompt();
-            }
-        }
-
-        private void ChangePrice()
-        {
-            uint id = AnsiConsole.Ask<uint>("Enter [green]shop id[/]");
-            string productName = AnsiConsole.Ask<string>("Enter [green]product name[/]");
-            uint price = AnsiConsole.Ask<uint>("Enter new [green]product price[/]");
-            try
-            {
-                _shopService.ChangePrice(id, productName, price);
-            }
-            catch (ShopServiceException e)
-            {
-                AnsiConsole.Render(new Markup($"[red]{e.Message}[/]\n"));
-                ReturnPrompt();
-            }
-        }
-
-        private void ReturnPrompt()
-        {
-            AnsiConsole.Render(new Text("Press Enter to return back"));
-            Console.Read();
+            return AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[yellow]Choose action[/]")
+                    .PageSize(10)
+                    .AddChoices(new[]
+                    {
+                        "Register product",
+                        "Register shop",
+                        "Add product to the shop",
+                        "Change price",
+                        "Find cheapest shop",
+                        "Buy",
+                        "Show shop products",
+                        "Show shops",
+                        "Show registered products",
+                        "Show my products",
+                        "Show my money",
+                        "Add money",
+                        "exit",
+                    }));
         }
     }
 }
