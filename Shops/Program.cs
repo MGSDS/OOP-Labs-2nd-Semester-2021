92eccd0758d19a156cedc6808ff6266daae3e476
Ivan Evtushenko <eid20021@gmail.com>
2021-09-16 19:42:51 +0300

    feat: ui added to Program.cs

--- Shops/Program.cs
+++ Shops/Program.cs
@@ -1,13 +1,16 @@
 ﻿using Shops.Entities;
 using Shops.Services;
-using Spectre.Console;
+using Shops.Ui;
 
 namespace Shops
 {
     internal class Program
     {
-        public static void Main()
+        private static void Main()
         {
+            uint money = 1000;
+            var ui = new UiRunner(new Buyer(money), new ShopService());
+            ui.Run();
         }
     }
 }
