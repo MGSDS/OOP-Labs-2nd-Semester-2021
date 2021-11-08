92c59a135663fe8f3494dd5916ca95c11a9998bc
Bill <eid20021@gmail.com>
2021-11-08 17:12:48 +0300

    fix: remove awful time provider logic

--- Banks/Providers/DateTimeProvider.cs
+++ Banks/Providers/DateTimeProvider.cs
@@ -4,22 +4,11 @@ namespace Banks.Providers
 {
     public class DateTimeProvider : IDateTimeProvider
     {
-        private DateTime _now;
-
         public DateTimeProvider()
         {
-            _now = DateTime.Now;
+            Now = DateTime.Now;
         }
 
-        public DateTime Now
-        {
-            get
-            {
-                DateTime res = _now;
-                _now = _now.AddSeconds(1);
-                return res;
-            }
-            set => _now = value;
-        }
+        public DateTime Now { get; set; }
     }
 }
